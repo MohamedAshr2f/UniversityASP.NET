9f512570bedd57cdcf5c73f89076ff75a8af1697
Mohamed Ashraf <mohamedashraf20191700504@gmail.com>
2025-09-23 10:37:12 +0300

    make Dependencies between Repositories

--- School.Infrastructure/ModuleInfrastructureDependencies.cs
+++ School.Infrastructure/ModuleInfrastructureDependencies.cs
@@ -1,7 +1,18 @@
-﻿namespace School.Infrastructure
-{
-    public class ModuleInfrastructureDependencies
-    {
+﻿using Microsoft.Extensions.DependencyInjection;
+using School.Infrastructure.Abstracts;
+using School.Infrastructure.Repositories;
 
+namespace School.Infrastructure
+{
+    public static class ModuleInfrastructureDependencies
+    {
+        public static IServiceCollection AddInfrastructureDependencies(this IServiceCollection services)
+        {
+            services.AddTransient<IDepartmentRepository, DepartmentRepository>();
+            services.AddTransient<IStudentRepository, StudentRepository>();
+
+            return services;
+
+        }
     }
 }
--- School.Infrastructure/Repositories/StudentRepository.cs
+++ School.Infrastructure/Repositories/StudentRepository.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using School.Infrastructure.Abstracts;
 
 namespace School.Infrastructure.Repositories
 {
-    internal class StudentRepository
+    public class StudentRepository : IStudentRepository
     {
     }
 }
