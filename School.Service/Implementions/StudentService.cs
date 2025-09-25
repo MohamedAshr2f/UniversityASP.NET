743f59ad35b78631fa1e70c0cbe3c332c00e4bcd
Mohamed Ashraf <mohamedashraf20191700504@gmail.com>
2025-09-25 15:25:13 +0300

    Nothing Changes

--- School.Service/Implementions/StudentService.cs
+++ School.Service/Implementions/StudentService.cs
@@ -8,7 +8,6 @@ namespace School.Service.Implementions
     public class StudentService : IStudentService
     {
         private readonly IStudentRepository _studentRepository;
-
         public StudentService(IStudentRepository studentRepository)
         {
             _studentRepository = studentRepository;
