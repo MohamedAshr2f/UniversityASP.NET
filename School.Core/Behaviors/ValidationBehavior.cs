904143932e5b12f2815248453681743781f6eb11
Mohamed Ashraf <mohamedashraf20191700504@gmail.com>
2025-09-28 14:29:16 +0300

    adding some localization

--- School.Core/Behaviors/ValidationBehavior.cs
+++ School.Core/Behaviors/ValidationBehavior.cs
@@ -1,5 +1,7 @@
 ﻿using FluentValidation;
 using MediatR;
+using Microsoft.Extensions.Localization;
+using School.Core.Resources;
 
 namespace School.Core.Behaviors
 {
@@ -7,9 +9,12 @@ namespace School.Core.Behaviors
         where TRequest : IRequest<TResponse>
     {
         private readonly IEnumerable<IValidator<TRequest>> _validators;
-        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+        private readonly IStringLocalizer<SharedResource> _stringLocalizer;
+
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators, IStringLocalizer<SharedResource> stringLocalizer)
         {
             _validators = validators;
+            _stringLocalizer = stringLocalizer;
         }
 
 
@@ -23,7 +28,7 @@ namespace School.Core.Behaviors
 
                 if (failures.Count != 0)
                 {
-                    var message = failures.Select(x => x.PropertyName + ": " + x.ErrorMessage).FirstOrDefault();
+                    var message = failures.Select(x => _stringLocalizer[$"{x.PropertyName}"] + ":" + _stringLocalizer[x.ErrorMessage]).FirstOrDefault();
 
                     throw new ValidationException(message);
 
--- School.Core/Features/Students/Command/Validations/AddStudentValidation.cs
+++ School.Core/Features/Students/Command/Validations/AddStudentValidation.cs
@@ -1,5 +1,7 @@
 ﻿using FluentValidation;
+using Microsoft.Extensions.Localization;
 using School.Core.Features.Students.Command.Models;
+using School.Core.Resources;
 using School.Service.Abstracts;
 
 namespace School.Core.Features.Students.Command.Validations
@@ -7,22 +9,24 @@ namespace School.Core.Features.Students.Command.Validations
     public class AddStudentValidation : AbstractValidator<AddStudentCommand>
     {
         private readonly IStudentService _studentService;
+        private readonly IStringLocalizer<SharedResource> _stringLocalizer;
 
-        public AddStudentValidation(IStudentService studentService)
+        public AddStudentValidation(IStudentService studentService, IStringLocalizer<SharedResource> stringLocalizer)
         {
             ApplyValidationRules();
             CustomValidation();
             _studentService = studentService;
+            _stringLocalizer = stringLocalizer;
         }
         public void ApplyValidationRules()
         {
-            RuleFor(s => s.Name).NotEmpty().WithMessage("Name Is Empty")
+            RuleFor(s => s.Name).NotEmpty().WithMessage(_stringLocalizer[SharedResourcesKey.NotEmpty])
                 .NotNull().WithMessage("Name Must be Not Null")
-                .MaximumLength(50).WithMessage("MaxLength is 50");
+                .MaximumLength(100).WithMessage(_stringLocalizer[SharedResourcesKey.MaxLengthis100]);
 
-            RuleFor(s => s.Address).NotEmpty().WithMessage("{PropertyName} Is Empty")
-               .NotNull().WithMessage("{PropertyValue} Name Must be Not Null")
-               .MaximumLength(50).WithMessage("{PropertyName} MaxLength is 50");
+            RuleFor(s => s.Address).NotEmpty().WithMessage(_stringLocalizer[SharedResourcesKey.NotEmpty])
+                .NotNull().WithMessage("{PropertyValue} Name Must be Not Null")
+                .MaximumLength(100).WithMessage(_stringLocalizer[SharedResourcesKey.MaxLengthis100]);
         }
         public void CustomValidation()
         {
