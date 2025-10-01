ce302b1640eaa9d7636b78b979d5f62b6a32ace6
Mohamed Ashraf <mohamedashraf20191700504@gmail.com>
2025-10-01 21:17:51 +0300

    Add Identity With USer Class

--- School.Infrastructure/ApplicationContext/Context.cs
+++ School.Infrastructure/ApplicationContext/Context.cs
@@ -1,10 +1,13 @@
 ﻿using System.Reflection;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using School.Data.Entities;
+using School.Data.Entities.Identity;
 
 namespace School.Infrastructure.ApplicationContext
 {
-    public class Context : DbContext
+    public class Context : IdentityDbContext<User, IdentityRole<int>, int, IdentityUserClaim<int>, IdentityUserRole<int>, IdentityUserLogin<int>, IdentityRoleClaim<int>, IdentityUserToken<int>>
     {
         public Context(DbContextOptions<Context> options) : base(options)
         {
@@ -16,6 +19,7 @@ namespace School.Infrastructure.ApplicationContext
             base.OnModelCreating(modelBuilder);
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
         }
+        public DbSet<User> Users { get; set; }
         public DbSet<Student> Students { get; set; }
         public DbSet<Department> Departments { get; set; }
         public DbSet<Subject> Subjects { get; set; }
---
+++ School.Infrastructure/ServiceRegisteration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace School.Infrastructure
+{
+    public static class ServiceRegisteration
+    {
+        public static IServiceCollection AddServiceRegisteration(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+
+            /*services.AddIdentity<User, Role>(option =>
+             {
+                 // Password settings.
+                 option.Password.RequireDigit = true;
+                 option.Password.RequireLowercase = true;
+                 option.Password.RequireNonAlphanumeric = true;
+                 option.Password.RequireUppercase = true;
+                 option.Password.RequiredLength = 6;
+                 option.Password.RequiredUniqueChars = 1;
+
+                 // Lockout settings.
+                 option.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                 option.Lockout.MaxFailedAccessAttempts = 5;
+                 option.Lockout.AllowedForNewUsers = true;
+
+                 // User settings.
+                 option.User.AllowedUserNameCharacters =
+                 "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                 option.User.RequireUniqueEmail = true;
+                 option.SignIn.RequireConfirmedEmail = true;
+
+             }).AddEntityFrameworkStores<ApplicationDBContext>().AddDefaultTokenProviders();*/
+        }
+    }
+}
